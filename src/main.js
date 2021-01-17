322d2a5fd27c6b15a2f738eafdd61ab54eac1513
Guillermo Alvarez <guillermo.alvarez@fiexact.com>
2021-01-17 10:57:57 -0700

    fix: authentication guard Se cambio el lugar donde se protegen las rutas en las que se debe estar autenticado al router. Con esto se evita un problema de que el laytout 'auth' es el default

--- src/main.js
+++ src/main.js
@@ -1,30 +1,18 @@
-import Vue from "vue"
-import App from "./App.vue"
-import Auth from "./layouts/auth.vue"
-import router from "./router"
-import store from "./store"
-import vuetify from "./plugins/vuetify"
-import "roboto-fontface/css/roboto/roboto-fontface.css"
-//https://jossef.github.io/material-design-icons-iconfont/
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from "vue";
+import App from "./App.vue";
+import Auth from "./layouts/auth.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+//https://jossef.github.io/material-design-icons-iconfont/ link para ver los iconos
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: (h) => {
-    // verificacion de que el token este seteado para continuar al app
-    if (store.getters.isTokenSet) {
-      // condicion para evitar error de redireccion a la ruta actual
-      if (router.currentRoute.fullPath != "/") router.push("") 
-      return h(App)
-    } 
-    // si no se direcciona al login
-    else {
-      router.push("login")
-      return h(Auth)
-    }
-  },
-}).$mount("#app")
+  render: (h) => h(App),
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Home from "../views/Home.vue"
+import store from "../store"
 
 Vue.use(VueRouter)
 
@@ -18,7 +19,7 @@ const routes = [
   {
     path: "/login",
     name: "Login",
-    component: () => import("@/views/Login"),
+    component: () => import("@/views/Login.vue"),
     meta: {
       layout: "auth",
     },
@@ -37,6 +38,12 @@ const routes = [
 
 const router = new VueRouter({
   routes,
+})
+
+// verificacion de que el token este seteado para continuar al app
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !store.getters.isTokenSet) next({ name: 'Login' })
+  else next()
 })
 
 export default router
