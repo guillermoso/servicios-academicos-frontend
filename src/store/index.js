152a51adb0bcf727439500f55bbfb1e8130c61a0
Guillermo Alvarez <gjalvarez09@gmail.com>
2021-01-16 19:26:47 -0700

    new vuex store file structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { state } from '@/store/state.js'
+import { getters } from '@/store/getters'
+import { mutations } from '@/store/mutations.js'
+import { actions } from '@/store/actions.js'
+import { auth } from '@/store/modules/auth.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  strict: true,
   modules: {
-  }
+    auth: auth,
+  },
+  state,
+  getters,
+  mutations,
+  actions
 })
