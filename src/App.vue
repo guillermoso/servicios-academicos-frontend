94b84784406967060680f498fac0f0b1b37e3611
Guillermo Alvarez <gjalvarez09@gmail.com>
2021-01-17 21:36:29 -0700

    made global alert component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
     <vue-extend-layouts />
+    <alert-message ref="alertMessage"/>
   </div>
 </template>
 
 <script>
 import VueExtendLayouts from 'vue-extend-layout'
+import alertMessage from '@/components/global/alerta';
+import Vue from 'vue';
+
 export default {
   name: 'App',
-  components: { VueExtendLayouts }
+  components: { VueExtendLayouts, alertMessage },
+   mounted() {
+    Vue.prototype.$message = this.$refs.alertMessage.show;
+  }
 }
 </script>
